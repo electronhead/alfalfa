ee7765c41257d0efa8d342ddd575cb372f462460
Gary Beaver <beaver@electronhead.com>
2021-02-10 13:19:05 -0800

    moved the local imported in front of importing the FastAPI main script

--- run.py
+++ run.py
@@ -1,6 +1,27 @@
+"""
+This script runs the api server with optionally supplied host and port.
+"""
+import argparse
 import uvicorn
+
+"""
+In this section import the modules that define Actions.
+These should be imported before importing whendo.api.main
+"""
+import alfalfa.action
+
+"""
+import the main script involving the creation of a FastAPI instance.
+"""
 from whendo.api import main
 
-import alfalfa.action
+
+"""
+parse command line arguments
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('-host', '--host', type=str, default='127.0.0.1', dest='host')
+parser.add_argument('-port', '--port', type=int, default=8000, dest='port')
+args = parser.parse_args()
 
 uvicorn.run(main.app, host='127.0.0.1', port=8000)
