e9b14cd2ec57c3d119091f3315584ae411417880
Gary Beaver <beaver@electronhead.com>
2021-02-10 13:54:20 -0800

    moved gpio class to alfalfa

---
+++ alfalfa/gpio_action.py
@@ -0,0 +1,42 @@
+"""
+These classes perform simple operations on pins.
+"""
+
+import RPi.GPIO as GPIO
+import whendo.core.util as util
+from whendo.core.action import Action
+
+class SetPin(Action):
+    """
+    For the specified pin, sets to HIGH if on=True, to LOW if on=False.
+    """
+    pin:int
+    on:bool
+
+    def execute(self, tag:str=None, scheduler_info:dict=None):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.HIGH if self.on else GPIO.LOW)
+        util.Output.pprint(lev_fil=util.Output.debug(), dictionary={'time':util.Now.s(), 'class':'SetPin', 'pin':self.pin, 'on':self.on, 'sched':scheduler_info})
+
+class TogglePin(Action):
+    """
+    For the specified pin, sets to HIGH if LOW, to LOW if HIGH.
+    """
+    pin:int
+
+    def execute(self, tag:str=None, scheduler_info:dict=None):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, not GPIO.input(self.pin))
+        util.Output.pprint(lev_fil=util.Output.debug(), dictionary={'time':util.Now.s(), 'class':'TogglePin', 'pin':self.pin, 'sched':scheduler_info})
+
+class Cleanup(Action):
+    """
+    Clean up the pins. See the docs for GPIO.cleanup().
+    """
+
+    def execute(self, tag:str=None, scheduler_info:dict=None):
+        GPIO.cleanup()
--- run.py
+++ run.py
@@ -8,13 +8,13 @@ import uvicorn
 In this section import the modules that define Actions.
 These should be imported before importing whendo.api.main
 """
+import alfalfa.gpio_action
 import alfalfa.action
 
 """
-import the main script involving the creation of a FastAPI instance.
+import the main script that creates the FastAPI instance (main.app).
 """
 from whendo.api import main
-
 
 """
 parse command line arguments
@@ -24,4 +24,7 @@ parser.add_argument('-host', '--host', type=str, default='127.0.0.1', dest='host
 parser.add_argument('-port', '--port', type=int, default=8000, dest='port')
 args = parser.parse_args()
 
-uvicorn.run(main.app, host='127.0.0.1', port=8000)
+"""
+uvicorn is the ASGI server that runs the api specified with FastAPI.
+"""
+uvicorn.run(main.app, host=args.host, port=args.port)
