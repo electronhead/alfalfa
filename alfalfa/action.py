1c6a3c07d9b8c00ead9dd7ad46ffabaad3eef893
Gary Beaver <beaver@electronhead.com>
2021-02-14 01:27:44 -0800

    reinstated references in action.py

--- alfalfa/action.py
+++ alfalfa/action.py
@@ -1,15 +1,18 @@
 """
 A proof of concept regarding imports from whengo library.
 """
+from typing import Optional
+from whendo.core.action import ActionPayload, Action
 from whendo.core.actions.file_action import FileHeartbeat
+from whendo.core.util import PP
+import logging
+
+logger = logging.getLogger(__name__)
 
 class AlfalfaAction(FileHeartbeat):
     color:str
     
     def execute(self, tag:str=None, scheduler_info:dict=None):
-        try:
-            info = scheduler_info if scheduler_info else {}
-            info['what?'] = 'Alfalfa rocks!'
-            super().execute(tag=tag, scheduler_info=info)
-        except Exception as exception:
-            return exception
+        info = scheduler_info if scheduler_info else {}
+        info['what?'] = 'Alfalfa rocks!'
+        super().execute(tag=tag, scheduler_info=info)
